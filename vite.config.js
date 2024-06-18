a2eeed87e7b17044bb1a8415c052cf0a6bdb4ed8
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-06-18 14:55:18 +0900

    chore: js to ts wip

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	root: '.',
+// 	build: {
+// 		outDir: 'dist',
+// 		rollupOptions: {
+// 			input: {
+// 				index: './index.html',
+// 				pitch: './pitch.html',
+// 				wave: './wave.html',
+// 			},
+// 		},
+// 	},
+// });
